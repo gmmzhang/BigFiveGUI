6b5ceeb4c8e93498ab08e221fa8cc7a2b102f8d8
Mq Zhang <37912077+imqzh@users.noreply.github.com>
2021-04-12 22:37:33 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        itemBank bank = new itemBank("Questions.txt");
+        ItemBank bank = new ItemBank("Questions.txt");
         MainWindow window = new MainWindow(bank);
     }
 }
