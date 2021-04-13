1a70623aa288b63ffdb89fdde7a1357310956b1e
Mq Zhang <37912077+imqzh@users.noreply.github.com>
2021-04-12 23:01:19 -0500

    Update IDandPasswords.java

--- src/IDandPasswords.java
+++ src/IDandPasswords.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class IDandPasswords {
     HashMap<String,String> logininfo = new HashMap<String, String>();
 
-    IDandPasswords(){
+    public IDandPasswords(){
         logininfo.put("Alta","pizza");
         logininfo.put("Lisa","apple");
         logininfo.put("Bruce","lee");
