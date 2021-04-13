86e1428a957cb3fbd9166185ddffb7044e2fe44a
Mq Zhang <37912077+imqzh@users.noreply.github.com>
2021-04-12 22:37:36 -0500

    Update MainWindow.java

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,32 +1,44 @@
 import javax.swing.*;
+import java.awt.*;
+
 public class MainWindow extends JFrame{
     private LoginPage loginPanel; // Declare a variable of JPanel type.
     private WelcomePage welcomePanel;
     private QuestionPanel questionPanel;
     private IDandPasswords idandPasswords;
-    private itemBank bank;
+    private ItemBank bank;
+    public int Height;
+    public int Width;
 
 
-    public MainWindow(itemBank bank) { // Constructor
-        this.bank = bank;// Contain items
+    public MainWindow(ItemBank itemBank) { // Constructor
+        super("Big Five Personality Assessment"); // Set the frame title
+        bank = itemBank;// Contain items
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close the frame when press X
+        Height = 500;
+        Width = 1000;
+        setSize(new Dimension(Width, Height)); // Set the frame size
+
         loginPanel = new LoginPage(this);  // Initialize JPanel object using the LoginPanel class
         add(loginPanel); // Add Panel Container
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack(); // Resize the frame to fit the components.
+        //pack(); // Resize the frame to fit the components.
     }
 
     public void userLoggedIn(String userID) {
         remove(loginPanel);
-        welcomePanel = new WelcomePage(userID);
+        welcomePanel = new WelcomePage(this, userID);
         add(welcomePanel);
         //pack();
     }
 
     public void userStarted(String userID) {
         remove(welcomePanel);
-        questionPanel = new QuestionPanel(bank);
+        questionPanel = new QuestionPanel(this, bank, userID);
         add(questionPanel);
+        getContentPane().invalidate();
+        getContentPane().validate();
         //pack();
     }
 
