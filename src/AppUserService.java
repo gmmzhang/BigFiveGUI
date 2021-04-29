c92d41c329d1f72d32d47510d5129f611eccbcfa
Mq Zhang <37912077+gmmzhang@users.noreply.github.com>
2021-04-28 22:55:39 -0500

    Create AppUserService.java

---
+++ src/AppUserService.java
@@ -0,0 +1,29 @@
+package com.example.demo.appuser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService, UserDetailsPasswordService {
+    private final static String USER_NOT_FOUND_MSG =
+            "user with email %s not found";
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+        throws UsernameNotFoundException{
+        return appUserRepository.findByEmail(email)
+            .orElseThrow(()-> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+
+    @Override
+    public UserDetails updatePassword(UserDetails userDetails, String s) {
+        return null;
+    }
+}
