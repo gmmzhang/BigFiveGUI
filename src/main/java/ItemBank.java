3fe89939792f5cab054d56691069e7324103d3cf
Mq Zhang <37912077+gmmzhang@users.noreply.github.com>
2021-05-13 22:39:57 -0500

    Create ItemBank.java

---
+++ src/main/java/ItemBank.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ItemBank {
+
+    ArrayList<Item> items;
+
+    public ItemBank(String fileName) throws FileNotFoundException{
+        items = new ArrayList<Item>();
+        try {
+            Scanner input = new Scanner(new File(fileName));
+            loadItem(input);
+        } catch (FileNotFoundException e) {
+            System.err.println("Could not find file: " + fileName);
+            throw e;
+        }
+    }
+
+    private void loadItem(Scanner input) {
+        while (input.hasNextLine()){
+            String line = input.nextLine();
+            Scanner scannerLine = new Scanner(line);
+            Item item = new Item(scannerLine);
+            items.add(item);
+            //System.out.println(item.getDescription());
+        }
+    }
+
+}
