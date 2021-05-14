9b0adac1363bc51ec8fb1f2d14f415d2d4291a75
Mq Zhang <37912077+gmmzhang@users.noreply.github.com>
2021-05-13 22:40:06 -0500

    Create ReportPanel.java

---
+++ src/main/java/ReportPanel.java
@@ -0,0 +1,198 @@
+import javax.swing.*;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.lang.Math.round;
+
+public class ReportPanel extends JPanel {
+    private static ArrayList<Double> finalWeights;
+    private MainFrame frame;
+    private String userID;
+    private int[] questionAnswer;
+    private JButton commentsButton;
+    private JTable table;
+    public static Object[][] userRecords;
+
+    public ReportPanel(MainFrame mainframe, String user, int[] answer, Object[][] records) {
+        frame = mainframe;
+        userID = user;
+        questionAnswer = answer;
+        userRecords = records;
+
+        finalWeights = new ArrayList<>();
+
+        finalWeights.add(getAvgE(reverse(questionAnswer)));
+        finalWeights.add(getAvgA(reverse(questionAnswer)));
+        finalWeights.add(getAvgC(reverse(questionAnswer)));
+        finalWeights.add(getAvgN(reverse(questionAnswer)));
+        finalWeights.add(getAvgI(reverse(questionAnswer)));
+
+        //System.out.println(finalWeights);
+
+        String[] columnNames = {"Category","Percentage"};
+
+        double sum = finalWeights.stream().mapToDouble(Double::doubleValue).sum();
+
+        userRecords = new Object[][]{
+                {"Extroversion", round(finalWeights.get(0)/sum*100)+"%"},
+                {"Agreeableness", round(finalWeights.get(1)/sum*100)+"%"},
+                {"Conscientiousness", round(finalWeights.get(2)/sum*100)+"%"},
+                {"Neuroticism", round(finalWeights.get(3)/sum*100)+"%"},
+                {"Intellect/Imagination", round(finalWeights.get(4)/sum*100)+"%"}
+        };
+
+        table = new JTable(userRecords, columnNames);
+        //System.out.println(Arrays.deepToString(userRecords));
+        System.out.print(Arrays.deepToString(getRecords()));
+
+        //add(table, BorderLayout.NORTH);
+        add(new JScrollPane(table));
+        add(new JScrollPane(createDemoPanel()));
+
+        frame.setSize(1500,500);
+
+        commentsButton = new JButton("Leave Feedback");
+        commentsButton.addActionListener(new CommentsButtonListener());
+        add(commentsButton);
+
+    }
+
+    public static Object[][] getRecords() {
+        return userRecords;
+    }
+
+    public int[] reverse(int[] arr1){
+        int[] arr2 = new int[20];
+        arr2[0] = arr1[0];
+        arr2[1] = arr1[1];
+        arr2[2] = arr1[2];
+        arr2[3] = arr1[3];
+        arr2[4] = arr1[4];
+        arr2[5] = 6- arr1[5];
+        arr2[6] = 6- arr1[6];
+        arr2[7] = 6- arr1[7];
+        arr2[8] = 6- arr1[8];
+        arr2[9] = 6- arr1[9];
+        arr2[10] = arr1[10];
+        arr2[11] = arr1[11];
+        arr2[12] = arr1[12];
+        arr2[13] = arr1[13];
+        arr2[14] = 6- arr1[14];
+        arr2[15] = 6- arr1[15];
+        arr2[16] = 6- arr1[16];
+        arr2[17] = 6- arr1[17];
+        arr2[18] = 6- arr1[18];
+        arr2[19] = 6- arr1[19];
+        return arr2;
+    }
+
+    int sumE;
+    double avgE;
+    int sumA;
+    double avgA;
+    int sumC;
+    double avgC;
+    int sumN;
+    double avgN;
+    int sumI;
+    double avgI;
+
+    public double getAvgE (int[] arr) {
+        for(int i=0; i<20; i+=5){
+            sumE += arr[i];
+            avgE = sumE/4.0;
+        }
+        return avgE;
+    }
+
+    public double getAvgA (int[] arr) {
+        for(int i=1; i<20; i+=5){
+            sumA += arr[i];
+            avgA = sumA/4.0;
+        }
+        return avgA;
+    }
+
+    public double getAvgC (int[] arr) {
+        for(int i=2; i<20; i+=5){
+            sumC += arr[i];
+            avgC = sumC/4.0;
+        }
+        return avgC;
+    }
+
+    public double getAvgN (int[] arr) {
+        for(int i=3; i<20; i+=5){
+            sumN += arr[i];
+            avgN = sumN/4.0;
+        }
+        return avgN;
+    }
+
+    public double getAvgI (int[] arr) {
+        for(int i=4; i<20; i+=5){
+            sumI += arr[i];
+            avgI = sumI/4.0;
+        }
+        return avgI;
+    }
+
+    private static PieDataset createDataset(ArrayList<Double> arr){
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("Extroversion", arr.get(0));
+        dataset.setValue("Agreeableness", arr.get(1));
+        dataset.setValue("Conscientiousness", arr.get(2));
+        dataset.setValue("Neuroticism", arr.get(3));
+        dataset.setValue("Intellect/Imagination", arr.get(4));
+        return dataset;
+    }
+
+    private static JFreeChart createChart(PieDataset dataset){
+        JFreeChart chart = ChartFactory.createPieChart(
+                "Big Five Personality",
+                dataset,
+                true,
+                true,
+                false
+        );
+        return chart;
+    }
+
+//    PiePlot plot = (PiePlot) createChart(createDataset(finalWeights)).getPlot();
+//    plot.setLabelGenerator(true);
+
+    public static JPanel createDemoPanel(){
+        JFreeChart chart = createChart(createDataset(finalWeights));
+
+        PiePlot plot = (PiePlot) chart.getPlot();
+        PieSectionLabelGenerator gen = new StandardPieSectionLabelGenerator(
+                "{0}:  {2}", new DecimalFormat("0"), new DecimalFormat("0%"));
+        plot.setLabelGenerator(gen);
+
+        return new ChartPanel(chart);
+    }
+
+    private class CommentsButtonListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            frame.leaveComments(userID);
+        }
+    }
+
+
+
+}
